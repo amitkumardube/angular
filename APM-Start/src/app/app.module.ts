fe708d28f622db2bbeacb23565aa02abbd022fc6
amitkumardube <amitkumardube@gmail.com>
2021-01-12 22:15:02 +0200

    service implementation

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,8 +4,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent} from './products/product-list.component';
-import { CovertToSpacesPipe} from './shared/convert-to-spaces.pipe'
-import { StarComponent } from './shared/star.component'
+import { CovertToSpacesPipe} from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { StarComponent } from './shared/star.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
